7ced3b8bb1990d2e0eb8fc4ac1368c5c114cab73
ADITYA <you@example.com>
2025-01-29 12:01:19 +0530

    pt2 code updated

---
+++ project1/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.amber,
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
